d018cb3338e4c9a2dda16e393259f9cb2a9b5deb
TNGPB <giaosuxao@gmail.com>
2021-12-21 20:36:27 +0700

    xoá

--- script.js
+++
@@ -1,2 +0,0 @@
-
-console.log("%c Ngày quay tay mấy lần ? %c", 'font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;font-size:24px;color:#00bbee;-webkit-text-fill-color:#00bbee;-webkit-text-stroke: 1px #00bbee;', "font-size:12px;color:#999999;");
