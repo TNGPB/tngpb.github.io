cdbb809d18abc4e734cc3ec6236a9b7e42bc04a9
TNGPB <giaosuxao@gmail.com>
2022-02-16 03:23:16 +0700

    up

---
+++ dulieu.js
@@ -0,0 +1,293 @@
+//Khai báo mã hàng 
+		// Cấu trúc " Mã hàng -> Vị trí -> Màu -> Cỡ "
+        var structure = [
+		               // mã hàng 205
+                {label:"class",value:"308205",child:[
+                    {label:"section",value:"Thân sau trái",child:[
+                        {label:"subject",value:"Black #967",child:[
+                            {label:"room",value:"2XS",child:[]},
+							{label:"room",value:"XS",child:[]},
+							{label:"room",value:"S",child:[]},
+							{label:"room",value:"M",child:[]},
+							{label:"room",value:"L",child:[]},
+							{label:"room",value:"XL",child:[]},
+							{label:"room",value:"2XL",child:[]},
+                        ]},
+                        {label:"subject",value:"Pink #544",child:[
+                            {label:"room",value:"2XS",child:[]},
+							{label:"room",value:"XS",child:[]},
+							{label:"room",value:"S",child:[]},
+							{label:"room",value:"M",child:[]},
+							{label:"room",value:"L",child:[]},
+							{label:"room",value:"XL",child:[]},
+							{label:"room",value:"2XL",child:[]},
+                        ]},
+                        {label:"subject",value:"Malanged #642",child:[
+                            {label:"room",value:"2XS",child:[]},
+							{label:"room",value:"XS",child:[]},
+							{label:"room",value:"S",child:[]},
+							{label:"room",value:"M",child:[]},
+							{label:"room",value:"L",child:[]},
+							{label:"room",value:"XL",child:[]},
+							{label:"room",value:"2XL",child:[]},
+                        ]},
+						{label:"subject",value:"Liberaty Coral #546",child:[
+                            {label:"room",value:"2XS",child:[]},
+							{label:"room",value:"XS",child:[]},
+							{label:"room",value:"S",child:[]},
+							{label:"room",value:"M",child:[]},
+							{label:"room",value:"L",child:[]},
+							{label:"room",value:"XL",child:[]},
+							{label:"room",value:"2XL",child:[]},
+                        ]},
+                    ]},
+                    {label:"section",value:"Cộp cạp",child:[
+                         {label:"subject",value:"Black #967",child:[
+                            {label:"room",value:"2XS",child:[]},
+							{label:"room",value:"XS",child:[]},
+							{label:"room",value:"S",child:[]},
+							{label:"room",value:"M",child:[]},
+							{label:"room",value:"L",child:[]},
+							{label:"room",value:"XL",child:[]},
+							{label:"room",value:"2XL",child:[]},
+                        ]},
+                        {label:"subject",value:"Pink #544",child:[
+                            {label:"room",value:"2XS",child:[]},
+							{label:"room",value:"XS",child:[]},
+							{label:"room",value:"S",child:[]},
+							{label:"room",value:"M",child:[]},
+							{label:"room",value:"L",child:[]},
+							{label:"room",value:"XL",child:[]},
+							{label:"room",value:"2XL",child:[]},
+                        ]},
+                        {label:"subject",value:"Malanged #642",child:[
+                            {label:"room",value:"2XS",child:[]},
+							{label:"room",value:"XS",child:[]},
+							{label:"room",value:"S",child:[]},
+							{label:"room",value:"M",child:[]},
+							{label:"room",value:"L",child:[]},
+							{label:"room",value:"XL",child:[]},
+							{label:"room",value:"2XL",child:[]},
+                        ]},
+						{label:"subject",value:"Liberaty Coral #546",child:[
+                            {label:"room",value:"2XS",child:[]},
+							{label:"room",value:"XS",child:[]},
+							{label:"room",value:"S",child:[]},
+							{label:"room",value:"M",child:[]},
+							{label:"room",value:"L",child:[]},
+							{label:"room",value:"XL",child:[]},
+							{label:"room",value:"2XL",child:[]},
+                        ]},
+                    ]},                   
+                ]},
+				// mã hàng749
+                {label:"class",value:"303749",child:[
+                    {label:"section",value:"Thân trước trái",child:[
+                        {label:"subject",value:"Black",child:[
+                           {label:"room",value:"5-6",child:[]},
+							{label:"room",value:"7-8",child:[]},
+							{label:"room",value:"8-9",child:[]},
+							{label:"room",value:"10-11",child:[]},
+							{label:"room",value:"12-13",child:[]},
+							{label:"room",value:"14-15",child:[]},							
+                        ]},                      
+                    ]},                   
+                ]},
+				// mã hàng 661
+                {label:"class",value:"322661",child:[
+                     {label:"section",value:"Thân sau trái",child:[
+                        {label:"subject",value:"Comics #155",child:[
+                            {label:"room",value:"2XS",child:[]},
+							{label:"room",value:"XS",child:[]},
+							{label:"room",value:"S",child:[]},
+							{label:"room",value:"M",child:[]},
+							{label:"room",value:"L",child:[]},
+							{label:"room",value:"XL",child:[]},
+							{label:"room",value:"2XL",child:[]},
+                        ]},
+                        {label:"subject",value:"Venice Blue #146",child:[
+                            {label:"room",value:"2XS",child:[]},
+							{label:"room",value:"XS",child:[]},
+							{label:"room",value:"S",child:[]},
+							{label:"room",value:"M",child:[]},
+							{label:"room",value:"L",child:[]},
+							{label:"room",value:"XL",child:[]},
+							{label:"room",value:"2XL",child:[]},
+                        ]},
+                        {label:"subject",value:"Green #144",child:[
+                            {label:"room",value:"2XS",child:[]},
+							{label:"room",value:"XS",child:[]},
+							{label:"room",value:"S",child:[]},
+							{label:"room",value:"M",child:[]},
+							{label:"room",value:"L",child:[]},
+							{label:"room",value:"XL",child:[]},
+							{label:"room",value:"2XL",child:[]},
+                        ]},
+						{label:"subject",value:"Stripe Blue #142",child:[
+                            {label:"room",value:"2XS",child:[]},
+							{label:"room",value:"XS",child:[]},
+							{label:"room",value:"S",child:[]},
+							{label:"room",value:"M",child:[]},
+							{label:"room",value:"L",child:[]},
+							{label:"room",value:"XL",child:[]},
+							{label:"room",value:"2XL",child:[]},
+                        ]},
+						 {label:"subject",value:"Stripe Black #535",child:[
+                            {label:"room",value:"2XS",child:[]},
+							{label:"room",value:"XS",child:[]},
+							{label:"room",value:"S",child:[]},
+							{label:"room",value:"M",child:[]},
+							{label:"room",value:"L",child:[]},
+							{label:"room",value:"XL",child:[]},
+							{label:"room",value:"2XL",child:[]},
+                        ]},
+                        {label:"subject",value:"Square black #147",child:[
+                            {label:"room",value:"2XS",child:[]},
+							{label:"room",value:"XS",child:[]},
+							{label:"room",value:"S",child:[]},
+							{label:"room",value:"M",child:[]},
+							{label:"room",value:"L",child:[]},
+							{label:"room",value:"XL",child:[]},
+							{label:"room",value:"2XL",child:[]},
+                        ]},
+                        {label:"subject",value:"Palm blue #149",child:[
+                            {label:"room",value:"2XS",child:[]},
+							{label:"room",value:"XS",child:[]},
+							{label:"room",value:"S",child:[]},
+							{label:"room",value:"M",child:[]},
+							{label:"room",value:"L",child:[]},
+							{label:"room",value:"XL",child:[]},
+							{label:"room",value:"2XL",child:[]},
+                        ]},
+						{label:"subject",value:"Newwaves Blue #088",child:[
+                            {label:"room",value:"2XS",child:[]},
+							{label:"room",value:"XS",child:[]},
+							{label:"room",value:"S",child:[]},
+							{label:"room",value:"M",child:[]},
+							{label:"room",value:"L",child:[]},
+							{label:"room",value:"XL",child:[]},
+							{label:"room",value:"2XL",child:[]},
+                        ]},
+						{label:"subject",value:"Palm Mint #090",child:[
+                            {label:"room",value:"2XS",child:[]},
+							{label:"room",value:"XS",child:[]},
+							{label:"room",value:"S",child:[]},
+							{label:"room",value:"M",child:[]},
+							{label:"room",value:"L",child:[]},
+							{label:"room",value:"XL",child:[]},
+							{label:"room",value:"2XL",child:[]},
+                        ]},
+						{label:"subject",value:"Palmitito Black #455",child:[
+                            {label:"room",value:"2XS",child:[]},
+							{label:"room",value:"XS",child:[]},
+							{label:"room",value:"S",child:[]},
+							{label:"room",value:"M",child:[]},
+							{label:"room",value:"L",child:[]},
+							{label:"room",value:"XL",child:[]},
+							{label:"room",value:"2XL",child:[]},
+                        ]},
+                    ]},
+                ]},
+				// mã hàng 046
+				{label:"class",value:"310046",child:[
+                    {label:"section",value:"Thân trước trái",child:[
+                        {label:"subject",value:"Black",child:[
+                            {label:"room",value:"2XS",child:[]},
+							{label:"room",value:"XS",child:[]},
+							{label:"room",value:"S",child:[]},
+							{label:"room",value:"M",child:[]},
+							{label:"room",value:"L",child:[]},
+							{label:"room",value:"XL",child:[]},
+							{label:"room",value:"2XL",child:[]},							
+                        ]},                       
+                    ]},
+                    {label:"section",value:"Thân sườn phải",child:[
+                        {label:"subject",value:"Black",child:[
+                            {label:"room",value:"2XS",child:[]},
+							{label:"room",value:"XS",child:[]},
+							{label:"room",value:"S",child:[]},
+							{label:"room",value:"M",child:[]},
+							{label:"room",value:"L",child:[]},
+							{label:"room",value:"XL",child:[]},
+							{label:"room",value:"2XL",child:[]},							
+                        ]},                       
+                    ]},                  
+                ]},
+				// mã hàng 654
+				{label:"class",value:"303654",child:[
+                    {label:"section",value:"Thân sau phải ",child:[
+                        {label:"subject",value:"Black",child:[
+                            {label:"room",value:"2XS",child:[]},
+							{label:"room",value:"XS",child:[]},
+							{label:"room",value:"S",child:[]},
+							{label:"room",value:"M",child:[]},
+							{label:"room",value:"L",child:[]},
+							{label:"room",value:"XL",child:[]},
+							{label:"room",value:"2XL",child:[]},							
+                        ]},                      
+                    ]},    
+                ]},
+            ];
+        //global variable declaration to store value stepwise
+        var cindex, sindex, jindex;
+        //calling funfction onload
+        window.onload = function(){
+            classstrucure(structure, document.querySelectorAll('[name=class]'));
+        }
+        document.querySelector('body').addEventListener('change',function(e){
+            if(e.target.tagName.toLowerCase() == "select"){
+                if(e.target.value!==""){
+                    //check of nextsibling is null or not 
+                    nextsibling = e.target.closest('DIV').nextSibling.nextSibling;
+                    if(nextsibling!==null){
+                        var _nameAttr=e.target.getAttribute("name");
+                        var nextElm = nextsibling.querySelectorAll('SELECT');
+                        //for your project only you have to change the attributes name
+                        if(_nameAttr==='class'){
+                            cindex = structure.map(function(i){return i.value}).indexOf(e.target.value);
+                            classstrucure(structure[cindex].child, nextElm);
+                        //for your project only you have to change the attributes name
+                        }else if(_nameAttr==='section'){
+                            sindex = structure[cindex].child.map(function(i){return i.value}).indexOf(e.target.value);
+                            classstrucure(structure[cindex].child[sindex].child, nextElm);
+                        //for your project only you have to change the attributes name
+                        }else if(_nameAttr==='subject'){
+                            jindex = structure[cindex].child[sindex].child.map(function(i){return i.value}).indexOf(e.target.value);
+                            classstrucure(structure[cindex].child[sindex].child[jindex].child, nextElm);
+                        }
+                    }
+                }else{
+                    //e.target.value is empty then same function will fire with defferent parameters
+                    clearsturcture(e.target.closest('DIV').nextSibling);
+                }
+            }
+        });
+        //function create the structure of class step wise
+        function classstrucure(data, el){
+            //first clear all the select element onload
+            //clear all next sibling accept itself
+            el.forEach(element => {
+                clearsturcture(element.closest('DIV')); 
+            });
+           // clearsturcture(el[0].closest('DIV'));            
+            var opt = '<option value="">Vui lòng chọn</option>';
+            //creating options elemnet
+            for(var i=0;i<data.length;i++){
+                opt += '<option value="'+data[i].value+'">'+data[i].value+'</option>';
+            }
+            //appending option on each element
+            el.forEach(element => {
+                element.innerHTML = opt;
+            });
+        }
+        //clear all the next drop down element after itself
+        function clearsturcture(parentEl){
+            while (parentEl) {
+                if(parentEl.tagName !== undefined){
+                    //appending initial option on every next sibling
+                    parentEl.querySelector('SELECT').innerHTML = '<option>Vui lòng chọn</option>';
+                }
+                //looping through last existing sibling and target
+                parentEl = parentEl.nextSibling;
+            }
+        }
